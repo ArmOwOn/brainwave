b8a6907f264d7803fb3b8573b5d27683a8a29a14
Armaan Seydi <armaanseydi@gmail.com>
2024-04-23 13:06:11 +0330

    button component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button href={"#login"}>Something</Button>
+      <ButtonGradient />
     </>
   );
 };
